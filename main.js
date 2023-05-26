9c9480df9562c73cc0f1b401e2da84f407b0d8ef
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2023-05-26 12:36:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+rightwristx=0;
+leftwristx=0;
+difference=0;
+
 function setup()
 {
     video=createCapture(VIDEO);
@@ -16,9 +20,17 @@ function gotPoses(results)
     if(results.length>0)
     {
         console.log(results);
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        difference=floor(leftwristx-rightwristx);
+        console.log("left wrist x = "+leftwristx+"right wrist x = "+rightwristx+"difference is "+difference);
     }
 }
 function draw()
 {
     background('#0cf7ec');
+    textSize(difference);
+    document.getElementById("span1").innerHTML="Size of text will be = "+ difference+"px";
+    fill('#fc1c03');
+    text('Harshit Malhotra',200,200)
 }
